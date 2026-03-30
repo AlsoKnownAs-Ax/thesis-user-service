ec57687dd8161c3e80a3ad2a1a4af97c1ba8542e
--Ax- <andreiamzu@yahoo.com>
2026-03-30 22:14:02 +0200

    feat: add user db context

---
+++ Infrastructure/UserDbContext.cs
@@ -0,0 +1,24 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) {}
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+           entity.HasKey(u => u.Id);
+           entity.HasIndex(u => u.Email).IsUnique();
+
+           entity.Property(u => u.Email).IsRequired();
+           entity.Property(u => u.FirstName).IsRequired();
+           entity.Property(u => u.LastName).IsRequired(); 
+        });
+    }
+}
